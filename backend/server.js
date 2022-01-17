989a4b632d344968867acf0bcc90b8b796c5cc1c
Senal Ransara <senal.ransara38@gmail.com>
2022-01-17 12:55:23 +0530

    Created Database Connection

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const PORT = 8000;
+const DB_URL = 'mongodb+srv://senal:senal123@recipe.cbqdg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+mongoose.connect(DB_URL).then(()=> {
+    console.log('DB Connected Successfully');
+})
+.catch((err) => console.log('DB Connection Error',err));
+
+
+app.listen(PORT,()=>{
+    console.log(`App is running on ${PORT}`);
+});
