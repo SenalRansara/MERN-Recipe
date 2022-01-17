e271bb1f29281ec61765315d3e040d4bcb49f1c1
Senal Ransara <senal.ransara38@gmail.com>
2022-01-17 14:32:41 +0530

    created the post method

---
+++ backend/Controller/recipeController.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+const Recipe = require("../model/recipeModel")
+//const {v4:} // have to add auto increment id
+
+router.post("/addRecipe",async (req,res)=>{
+    //const recId = //add auto increment id
+    const recName = req.body.recName;
+    const recIng = req.body.recIng;
+    const recDes = req.body.recDes;
+
+const newRecipe = new Recipe({
+    //recId,
+    recName,
+    recIng,
+    recDes
+})
+try {
+    Recipe.find({ nic: newRecipe.nic }, async (err, doc) => {
+        if (Object.keys(doc).length == 0) {
+
+            try {
+                let response = await newRecipe.save();
+                if (response)
+                    //console.log(doc);
+                    return res.status(201).send({ message: "new Recipe Added" });
+            } catch (err) {
+                //console.log("error while saving");
+                return res.status(500).send({ status: "Internal Server Error" });
+            }
+        }
+        else {
+            //console.log("nic found")
+            return res.status(400).send({ status: "User already exist!" });
+        }
+    });
+} catch (err) {
+    console.log("error", err)
+}
+});
---
+++ backend/model/recipeModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const RecipeSchema = new Schema({
+
+    recipeId: { type: String },
+    recipeName: { type: String },
+    ingredients: { type: String },
+    description: {type: String}
+
+});
+
+const  Recipe = mongoose.model('Recipe',RecipeSchema)
+module.exports = Recipe;
