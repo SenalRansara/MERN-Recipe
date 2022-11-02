bb1356ee7fb7f8587cec8036073e5fc6dc2de71f
SenalRansara <senal.ransara38@gmail.com>
2022-11-02 23:22:11 +0530

    commit for prev date

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,18 @@
 // this is app.js file 
+//2022-11-2
 
 import React from "react";
-import { BrowserRouter as Router, Route,Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/view/recipeView";
 import Header from "./components/Header";
 import View from "./components/view/ViewRecipe";
 
 function App() {
   return (
-    
+
     <Router>
-      <Header/>
-      
+      <Header />
+
       <Routes>
         {/* <Home /> */}
         <Route path="/" element={<Home />}></Route>
